44399ccea542869e716ce9c826c9d6e57201b224
AsifurRahman66544 <93776889+AsifurRahman66544@users.noreply.github.com>
2022-03-01 16:27:22 +0600

    calling is complete

--- js/app.js
+++ js/app.js
@@ -14,17 +14,16 @@ const searchPhone = () => {
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     data.forEach(phone => {
-        console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
          <div class="card">
-             <img src="..." class="card-img-top" alt="...">
+             <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                     to
-                     additional content. This content is a little bit longer.</p>
+                <p class="card-text">${phone.brand}</p>
+                 <h5 class="card-title">${phone.phone_name}</h5>
+                 <button class="btn-style">Explore</button>
+                     
            </div>
          </div>
          `;
