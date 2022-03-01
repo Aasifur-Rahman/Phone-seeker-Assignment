f570aa24fd35bf7bacff9e72c4c36b36cda4a2c2
AsifurRahman66544 <93776889+AsifurRahman66544@users.noreply.github.com>
2022-03-01 14:35:08 +0600

    halka dynamic done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
-
+    // console.log(searchText);
     searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data));
+}
+
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+         <div class="card">
+             <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                     to
+                     additional content. This content is a little bit longer.</p>
+           </div>
+         </div>
+         `;
+        searchResult.appendChild(div);
+    })
 }
